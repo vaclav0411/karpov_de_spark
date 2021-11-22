cebc557dee9b3f117346f0e8020931a5a42423a8
Maxim Samsonov <mvsamsonov@gmail.com>
2021-11-22 15:58:56 +0300

    Calculate mean check and tips ratio, save to postgres

--- main.py
+++ main.py
@@ -1,154 +1,121 @@
-import os
-from pathlib import Path
-
-from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql import functions as f
-from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType, DoubleType
-
-# MySql
-# host: rc1c-0yx6egw73a44epbu.mdb.yandexcloud.net
-# database: spark
-# user: developer
-# password: NxZ58QAgu8bfnSbt
-
+from pyspark.sql.types import (
+    DoubleType,
+    IntegerType,
+    StringType,
+    StructField,
+    StructType,
+    TimestampType,
+)
 
 # поля справочника
-dim_columns = ['id', 'name']
+dim_columns = ["id", "name"]
 
 vendor_rows = [
-    (1, 'Creative Mobile Technologies, LLC'),
-    (2, 'VeriFone Inc'),
+    (1, "Creative Mobile Technologies, LLC"),
+    (2, "VeriFone Inc"),
 ]
 
 rates_rows = [
-    (1, 'Standard rate'),
-    (2, 'JFK'),
-    (3, 'Newark'),
-    (4, 'Nassau or Westchester'),
-    (5, 'Negotiated fare'),
-    (6, 'Group ride'),
+    (1, "Standard rate"),
+    (2, "JFK"),
+    (3, "Newark"),
+    (4, "Nassau or Westchester"),
+    (5, "Negotiated fare"),
+    (6, "Group ride"),
 ]
 
 payment_rows = [
-    (1, 'Credit card'),
-    (2, 'Cash'),
-    (3, 'No charge'),
-    (4, 'Dispute'),
-    (5, 'Unknown'),
-    (6, 'Voided trip'),
+    (1, "Credit card"),
+    (2, "Cash"),
+    (3, "No charge"),
+    (4, "Dispute"),
+    (5, "Unknown"),
+    (6, "Voided trip"),
 ]
 
-trips_schema = StructType([
-    StructField('vendor_id', StringType(), True),
-    StructField('tpep_pickup_datetime', TimestampType(), True),
-    StructField('tpep_dropoff_datetime', TimestampType(), True),
-    StructField('passenger_count', IntegerType(), True),
-    StructField('trip_distance', DoubleType(), True),
-    StructField('ratecode_id', IntegerType(), True),
-    StructField('store_and_fwd_flag', StringType(), True),
-    StructField('pulocation_id', IntegerType(), True),
-    StructField('dolocation_id', IntegerType(), True),
-    StructField('payment_type', IntegerType(), True),
-    StructField('fare_amount', DoubleType(), True),
-    StructField('extra', DoubleType(), True),
-    StructField('mta_tax', DoubleType(), True),
-    StructField('tip_amount', DoubleType(), True),
-    StructField('tolls_amount', DoubleType(), True),
-    StructField('improvement_surcharge', DoubleType(), True),
-    StructField('total_amount', DoubleType(), True),
-    StructField('congestion_surcharge', DoubleType()),
-])
+trips_schema = StructType(
+    [
+        StructField("vendor_id", StringType(), True),
+        StructField("tpep_pickup_datetime", TimestampType(), True),
+        StructField("tpep_dropoff_datetime", TimestampType(), True),
+        StructField("passenger_count", IntegerType(), True),
+        StructField("trip_distance", DoubleType(), True),
+        StructField("ratecode_id", IntegerType(), True),
+        StructField("store_and_fwd_flag", StringType(), True),
+        StructField("pulocation_id", IntegerType(), True),
+        StructField("dolocation_id", IntegerType(), True),
+        StructField("payment_type", IntegerType(), True),
+        StructField("fare_amount", DoubleType(), True),
+        StructField("extra", DoubleType(), True),
+        StructField("mta_tax", DoubleType(), True),
+        StructField("tip_amount", DoubleType(), True),
+        StructField("tolls_amount", DoubleType(), True),
+        StructField("improvement_surcharge", DoubleType(), True),
+        StructField("total_amount", DoubleType(), True),
+        StructField("congestion_surcharge", DoubleType()),
+    ]
+)
 
 
-def agg_calc(spark: SparkSession) -> DataFrame:
-    data_path = os.path.join(Path(__name__).parent, 'data', '*.csv')
-
-    trip_fact = spark.read \
-        .option("header", "true") \
-        .schema(trips_schema) \
-        .csv(data_path)
-
-    datamart = trip_fact \
-        .where(trip_fact['vendor_id'].isNotNull()) \
-        .groupBy(trip_fact['vendor_id'],
-                 trip_fact['payment_type'],
-                 trip_fact['ratecode_id'],
-                 f.to_date(trip_fact['tpep_pickup_datetime']).alias('dt')
-                 ) \
-        .agg(f.sum(trip_fact['total_amount']).alias('sum_amount'), f.avg(trip_fact['tip_amount']).alias("avg_tips")) \
-        .select(f.col('dt'),
-                f.col('vendor_id'),
-                f.col('payment_type'),
-                f.col('ratecode_id'),
-                f.col('sum_amount'),
-                f.col('avg_tips')) \
-        .orderBy(f.col('dt').desc(), f.col('vendor_id'))
-
-    return datamart
-
-
-def create_dict(spark: SparkSession, header: list[str], data: list):
-    """создание словаря"""
-    df = spark.createDataFrame(data=data, schema=header)
-    return df
-
-
-def save_to_mysql(host: str, port: int, db_name: str, username: str, password: str, df: DataFrame, table_name: str):
+def save_to_postgres(
+    host: str,
+    port: int,
+    db_name: str,
+    username: str,
+    password: str,
+    df: DataFrame,
+    table_name: str,
+):
     props = {
-        'user': f'{username}',
-        'password': f'{password}',
-        'driver': 'com.mysql.cj.jdbc.Driver',
-        "ssl": "true",
-        "sslmode": "none",
+        "user": f"{username}",
+        "password": f"{password}",
+        "driver": "org.postgresql.Driver",
     }
 
-    df.write.jdbc(
-        url=f'jdbc:mysql://{host}:{port}/{db_name}',
+    df.write.mode("overwrite").jdbc(
+        url=f"jdbc:postgresql://{host}:{port}/{db_name}",
         table=table_name,
-        properties=props)
-
-
-def main(spark: SparkSession):
-    vendor_dim = create_dict(spark, dim_columns, vendor_rows)
-    payment_dim = create_dict(spark, dim_columns, payment_rows)
-    rates_dim = create_dict(spark, dim_columns, rates_rows)
-
-    datamart = agg_calc(spark)
-    datamart.show()
-
-    joined_datamart = datamart \
-        .join(other=vendor_dim, on=vendor_dim['id'] == f.col('vendor_id'), how='inner') \
-        .join(other=payment_dim, on=payment_dim['id'] == f.col('payment_type'), how='inner') \
-        .join(other=rates_dim, on=rates_dim['id'] == f.col('ratecode_id'), how='inner') \
-        .select(f.col('dt'),
-                f.col('vendor_id'), f.col('payment_type'), f.col('ratecode_id'), f.col('sum_amount'),
-                f.col('avg_tips'),
-                rates_dim['name'].alias('rate_name'), vendor_dim['name'].alias('vendor_name'),
-                payment_dim['name'].alias('payment_name'),
-                )
-
-    joined_datamart.show()
-
-    mysql_host = 'rc1c-0yx6egw73a44epbu.mdb.yandexcloud.net'
-    mysql_port = 3306
-    mysql_db = 'spark'
-    mysql_user = 'developer'
-    mysql_password = 'NxZ58QAgu8bfnSbt'
-
-    save_to_mysql(
-        host=mysql_host,
-        port=mysql_port,
-        db_name=mysql_db,
-        username=mysql_user,
-        password=mysql_password,
-        df=joined_datamart,
-        table_name='spark.my_data_mart'
+        properties=props,
     )
 
 
-if __name__ == '__main__':
-    main(SparkSession.
-         builder
-         .config("spark.jars", "jars/mysql-connector-java-8.0.25.jar")
-         .appName('My first spark job')
-         .getOrCreate())
+def main(spark: SparkSession):
+    data_path = "data/yellow_tripdata_2020-04.csv"
+    trip_fact = spark.read.option("header", "true").schema(trips_schema).csv(data_path)
+
+    trip_fact = (
+        trip_fact.filter("total_amount >= 0")
+        .withColumn("week", f.weekofyear("tpep_pickup_datetime"))
+        .withColumn("tips_to_total", f.col("tip_amount") / f.col("total_amount"))
+    )
+
+    result = trip_fact.groupby("week", "payment_type").agg(
+        f.mean("total_amount").alias("avg_check"),
+        f.mean("tips_to_total").alias("avg_tips_to_total"),
+    )
+
+    gp_host = "greenplum.lab.karpov.courses"
+    gp_port = 6432
+    gp_db = "students"
+    gp_user = "student"
+    gp_password = "Wrhy96_09iPcreqAS"
+
+    save_to_postgres(
+        host=gp_host,
+        port=gp_port,
+        db_name=gp_db,
+        username=gp_user,
+        password=gp_password,
+        df=result,
+        table_name="spark_practice_samsonov",
+    )
+
+
+if __name__ == "__main__":
+    main(
+        SparkSession.builder.config("spark.jars", "jars/postgresql-42.3.1.jar")
+        .appName("spark-homework")
+        .getOrCreate()
+    )
