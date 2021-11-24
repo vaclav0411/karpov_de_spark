9ebbf3e2564f2e242ae604fa7172f763949aed85
Anton Pilipenko <anton.pilipenko@instamart.ru>
2021-11-24 16:34:58 +0300

    MR sources added

---
+++ practice2/README.md
@@ -0,0 +1,32 @@
+# Практика MapReduce
+
+## Подготовка
+
+* создадим кластер
+* установим AWS CLI на одной из машин кластера:
+  ```shell
+   apt install -y awscli
+  ```
+* скачаем тестовые данные:
+  ```shell
+    wget -O alice.txt https://www.gutenberg.org/files/11/11-0.txt
+    wget -O frank.txt https://www.gutenberg.org/files/84/84-0.txt
+  ```
+* Создадим папки на HDFS:
+  ```shell
+  hadoop fs -mkdir /user/root
+  hadoop fs -mkdir /user/root/input-data
+  hadoop fs -put *.txt input-data/
+  ```  
+## Разберем исходный код mapper и reducer
+## Запустим код
+* Скопируем код на кластер
+  ```shell
+  scp ./*.py root@84.201.188.76:/tmp/mapreduce/ && scp ./run.sh root@84.201.188.76:/tmp/mapreduce/
+  ```
+* Запустим код
+  ```shell
+  cd /tmp/mapreduce/
+  chmod +x ./run.sh
+  ./run.sh 
+  ```
---
+++ practice2/run.sh
@@ -0,0 +1,30 @@
+export MR_OUTPUT=/user/root/output-data
+
+hadoop fs -rm -r $MR_OUTPUT
+
+hadoop jar "$HADOOP_MAPRED_HOME"/hadoop-streaming.jar \
+-Dmapred.job.name='Simple streaming job reduce 1' \
+-file /tmp/mapreduce/mapper.py -mapper /tmp/mapreduce/mapper.py \
+-file /tmp/mapreduce/reducer.py -reducer /tmp/mapreduce/reducer.py \
+-input /user/root/2019 -output $MR_OUTPUT
+
+
+
+#-Dmapred.job.name='Simple streaming job' \
+#-Dmapred.reduce.tasks=1 -Dmapreduce.input.lineinputformat.linespermap=1000 \
+#-inputformat org.apache.hadoop.mapred.lib.NLineInputFormat \
+
+
+# -inputformat org.apache.hadoop.mapred.lib.NLineInputFormat -Dmapreduce.input.lineinputformat.linespermap=1000 \
+
+## s3
+# -Dfs.s3a.endpoint=s3.amazonaws.com -Dfs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider
+
+#hadoop jar "$HADOOP_MAPRED_HOME"/hadoop-streaming.jar \
+#-Dmapred.job.name='Simple streaming job' \
+#-Dmapred.reduce.tasks=1 \
+#-Dfs.s3a.endpoint=s3.amazonaws.com -Dfs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider \
+#-inputformat org.apache.hadoop.mapred.lib.NLineInputFormat \
+#-file /tmp/mapreduce/mapper.py -mapper /tmp/mapreduce/mapper.py \
+#-file /tmp/mapreduce/reducer.py -reducer /tmp/mapreduce/reducer.py \
+#-input  s3a://nyc-tlc/trip\ data/yellow_tripdata_2019-1*  -output taxi-output
