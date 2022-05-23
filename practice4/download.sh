717d237ff289f2d9e0a1f57b247b2cea9aa34bd6
Anton Pilipenko <anton.pilipenko@instamart.ru>
2022-05-23 15:35:16 +0300

    s3-path-fix

--- practice4/download.sh
+++ practice4/download.sh
@@ -1,4 +1,4 @@
-aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2020-04.csv ./data/ --no-sign-request
+aws s3 cp s3://nyc-tlc/csv_backup/yellow_tripdata_2020-04.csv ./data/ --no-sign-request
 
 
 mkdir ~/.mysql && \                                                                                                        [2]
